b6d40d51e10d28e5ed02e99f61081d908e702b11
liweixin <627632598@qq.com>
2016-03-16 16:46:08 +0800

    1.在UserInfoActivity中显示用户信息。 2.侧边栏的头像和title换成真的。 (用户信息和侧边栏头像有一定几率无法显示，可能是网络连接异常的原因)

--- app/src/main/java/io/dongyue/gitlabandroid/activity/UserInfoActivity.java
+++ app/src/main/java/io/dongyue/gitlabandroid/activity/UserInfoActivity.java
@@ -7,10 +7,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import io.dongyue.gitlabandroid.R;
+import io.dongyue.gitlabandroid.model.api.UserFull;
+import io.dongyue.gitlabandroid.network.GitlabClient;
+import io.dongyue.gitlabandroid.network.GitlabSubscriber;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class UserInfoActivity extends AppCompatActivity {
+
+    @Bind(R.id.text)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,8 @@ public class UserInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        loadData();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +42,33 @@ public class UserInfoActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void loadData(){
+        Observable<UserFull> observable = GitlabClient.getInstance().getThisUser();
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new GitlabSubscriber<UserFull>() {
+                    @Override
+                    public void onNext(UserFull userFull) {
+                        StringBuilder st = new StringBuilder();
+                        st.append("User name: ").append(userFull.getUsername())
+                                .append("\nEmail: ").append(userFull.getEmail())
+                                .append("\nId: ").append(userFull.getId())
+                                .append("\nCreatedAt: ").append(userFull.getCreatedAt())
+                                .append("\nProjectsLimit: ").append(userFull.getProjectsLimit())
+                                .append("\nCurrentSignInAt: ").append(userFull.getCurrentSignInAt())
+                                .append("\nName: ").append(userFull.getName())
+                                .append("\nState: ").append(userFull.getState())
+                                .append("\nColorSchemeId: ").append(userFull.getColorSchemeId())
+                                .append("\nTwitter: ").append(userFull.getTwitter())
+                                .append("\nSkype: ").append(userFull.getSkype())
+                                .append("\nLinkedin: ").append(userFull.getLinkedin())
+                                .append("\nThemeId: ").append(userFull.getThemeId())
+                                .append("\nIdentities: ").append(userFull.getIdentities());
+                        textView.setText(st);
+                    }
+                });
     }
 
     @Override
--- app/src/main/java/io/dongyue/gitlabandroid/view/GitlabNavigationView.java
+++ app/src/main/java/io/dongyue/gitlabandroid/view/GitlabNavigationView.java
@@ -6,38 +6,67 @@ import android.support.design.widget.NavigationView;
 import android.util.AttributeSet;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import io.dongyue.gitlabandroid.R;
-import io.dongyue.gitlabandroid.activity.HomeActivity;
-import io.dongyue.gitlabandroid.activity.IssueActivity;
-import io.dongyue.gitlabandroid.model.api.Issue;
+import io.dongyue.gitlabandroid.model.api.UserFull;
+import io.dongyue.gitlabandroid.network.GitlabClient;
+import io.dongyue.gitlabandroid.network.GitlabSubscriber;
 import io.dongyue.gitlabandroid.utils.NavigationManager;
 import io.dongyue.gitlabandroid.utils.eventbus.RxBus;
 import io.dongyue.gitlabandroid.utils.eventbus.events.CloseDrawerEvent;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by Brotherjing on 2016/3/6.
  */
 public class GitlabNavigationView extends NavigationView {
 
+    @Bind(R.id.imageView)
+    ImageView imageView;
+    @Bind(R.id.name)
+    TextView name;
+    @Bind(R.id.email)
+    TextView email;
+
     public GitlabNavigationView(Context context) {
         super(context);
-        init();
+        init(context);
     }
 
     public GitlabNavigationView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context);
     }
 
     public GitlabNavigationView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
-    public void init(){
+    public void init(Context context){
         inflateMenu(R.menu.activity_home_drawer);
         View header = inflateHeaderView(R.layout.nav_header_home);
+        ButterKnife.bind(this, header);
+        Observable<UserFull> observable = GitlabClient.getInstance().getThisUser();
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new GitlabSubscriber<UserFull>() {
+                    @Override
+                    public void onNext(UserFull userFull) {
+                        name.setText(userFull.getName());
+                        email.setText(userFull.getEmail());
+                        GitlabClient.getPicasso().with(context)
+                                .load(userFull.getAvatarUrl())
+                                .resize(100, 100)  //图片大小还未确定
+                                .centerCrop()
+                                .into(imageView);
+                    }
+                });
         ButterKnife.bind(this, header);
         header.setOnClickListener(new OnClickListener() {
             @Override
