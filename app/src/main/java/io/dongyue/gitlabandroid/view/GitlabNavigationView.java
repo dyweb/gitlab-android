9807477369bc38408b1a3c4a5a40d1134f23a1d3
liweixin <627632598@qq.com>
2016-03-16 17:21:01 +0800

    修复上一个错误（打多一行butterknife...）

--- app/src/main/java/io/dongyue/gitlabandroid/view/GitlabNavigationView.java
+++ app/src/main/java/io/dongyue/gitlabandroid/view/GitlabNavigationView.java
@@ -67,7 +67,6 @@ public class GitlabNavigationView extends NavigationView {
                                 .into(imageView);
                     }
                 });
-        ButterKnife.bind(this, header);
         header.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
